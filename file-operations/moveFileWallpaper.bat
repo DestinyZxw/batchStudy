01ea08226d71963470583433a46edefff2858530
ZZZxw <destiny_zxw@sina.cn>
2023-06-27 06:56:43 +0800

    Update moveFileWallpaper.bat
    1,fix: bat与小红车资源目录不在同一盘符时，使用cd命令会被拒绝，更改为cd /d。
    2,upd: 冗余文件最终转移的地址由 C:\WallpaperBack ，更改为与小红车资源目录相同的盘符下面， %~d0\WallpaperBack
    3,upd: 将bat打开的编码格式设置为utf-8
    4,upd: 开启延迟拓展，文件操作命令执行完成后获取命令执行结果

--- file-operations/moveFileWallpaper.bat
+++ file-operations/moveFileWallpaper.bat
@@ -1,23 +1,29 @@
 @echo off
 
+chcp 65001
+
 set @=有问题联系&set #=Q&set/az=0x3204de4a
 title %@% +%#%%#% %z%
 
 @REM 设置wallpaper根目录,431960为固定的steamId
-SET WORK_PATH=C:\SteamLibrary\steamapps\workshop\content\431960
+SET WORK_PATH=D:\Play\Steam\steamapps\workshop\content\431960
 
 @REM 进入目标地址
-cd %WORK_PATH%
+cd /d %WORK_PATH%
+
+echo 检查：当前盘符： %~d0
 
 @REM 确认所操作的当前地址
-echo WORK_PATH is %WORK_PATH%
+echo 检查：所操作的当前地址 is %WORK_PATH%
 
 @REM 设置想要查找的文件
 SET File=project.json
 
 @REM 生成暂存地址
-set Recycle=C:\WallpaperBack
+set Recycle=%~d0\WallpaperBack
 md %Recycle%
+
+setlocal enabledelayedexpansion
 
 @REM 遍历子目录
 for /f "delims=" %%a in ('dir /ad/b') do (
@@ -30,16 +36,21 @@ for /f "delims=" %%a in ('dir /ad/b') do (
         @REM 我的cmd有设置默认编码,所以每次新建cmd时都会有这种顶部信息;
         @REM 但是不清楚为什么输入dir后，遍历后的第一个%a中也有存在;
         if exist %%a (
-            echo;target》 %WORK_PATH%\%%a   》》》》》》》》》》》》》》》》%Recycle%          
+            echo 执行： %WORK_PATH%\%%a   》move》 %Recycle%          
             move %WORK_PATH%\%%a %Recycle%
-            if %errorlevel% NEQ 0 echo error》%errorlevel%
-            if %errorlevel% EQU 0 echo %%a move complete 
+            
+            rem if !errorlevel! NEQ 0 echo error 》 !errorlevel!
+            rem if !errorlevel! EQU 0 echo %%a move complete 
+
+            if NOT ERRORLEVEL 1 (echo file was successfully archived) else (echo file was not archived)
+            
         ) ELSE (
             @REM echo %%a需要删除的目录不存在
         )   
     )
 )
 
-echo 冗余文件已全部移至%Recycle%
+echo;
+echo 检查：移动操作流程已结束，请到 %Recycle% 查看
 echo;%@% +%#%%#% %z%
 pause
